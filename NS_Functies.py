1f63067c7dd748362bb7b9cfe0b17fb25c3ecfe6
Jesse_Pols <jessepols@gmail.com>
2018-09-24 11:51:44 +0200

    Added new assignments

--- NS_Functies.py
+++ NS_Functies.py
@@ -31,13 +31,31 @@ def ritprijs(leeftijd, weekendrit, afstandKM):
 
     return round(prijs, 2)
 
+def Test(leeftijd, weekendrit, afstand):
+    print('\n--- Nieuwe test-situatie ---')
+    print('Afstand = ' + str(afstand) + ' km')
+    print('Leeftijd = ' + str(leeftijd) + ' jaar oud')
+    print('Weekend = ' + str(weekendrit))
 
-afstandKM = int(input("Afstand in kilometers: "))
-leeftijd = int(input("Leeftijd: "))
-weekendrit = str(input("Weekend (Ja / Nee): ")).lower()
-if weekendrit == "ja":
-    weekendrit = True
-else:
-    weekendrit = False
+    print("€ " + str(ritprijs(leeftijd, weekendrit, afstand)))
 
-print("€ " + str(ritprijs(leeftijd, weekendrit, afstandKM)))
+# De leeftijd, of het weekend is en het aantal kilometers:
+Test(18, True, 15)
+Test(20, True, 52)
+Test(25, False, 15)
+Test(40, False, 70)
+Test(12, True, 100)
+Test(8, False, 5)
+Test(69, True, 80)
+Test(80, False, 2)
+
+
+## afstandKM = int(input("Afstand in kilometers: "))
+## leeftijd = int(input("Leeftijd: "))
+## weekendrit = str(input("Weekend (Ja / Nee): ")).lower()
+## if weekendrit == "ja":
+##     weekendrit = True
+## else:
+##     weekendrit = False
+
+## print("€ " + str(ritprijs(leeftijd, weekendrit, afstandKM)))
--- pe7_4.py
+++ pe7_4.py
@@ -1,9 +1,31 @@
-def Add():
-   print("Add")
+import sys
 
-def Create():
-    print('Create')
+def Menu():
+    i = int(input('Kies een optie:\n1. Nieuwe naam toevoegen\n2. Huidige lijst inzien\n3. Sluiten\n'))
+    if i == 1:
+        NewName()
+    if i == 2:
+        ReadNames()
+    if i == 3:
+        sys.exit(0)
+    if i != 1 or i != 2 or i != 3:
+        print('\nDeze optie bestaat niet.')
+        Menu()
 
-def Input():
-    print('Welke naam wilt u toevoegen?')
-    print('Laat dit veld leeg als u het programma wilt sluiten.')
+def NewName():
+    new_name = input('Welke naam wilt u toevoegen?\n')
+    with open('hardlopers.txt', 'a') as f:
+        f.write(new_name + '\n')
+        print('{} succesvol toegevoegd.\n'.format(new_name))
+    Menu()
+
+def ReadNames():
+    try:
+        with open('hardlopers.txt', 'r') as f:
+            print(f.read())
+    except:
+        print('Het bestand bestaat niet.\n')
+    Menu()
+
+
+Menu()
