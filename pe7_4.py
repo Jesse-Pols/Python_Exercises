fca26aecc1285dfce6dd90e8f2909ec32339f77f
Jesse_Pols <jessepols@gmail.com>
2018-09-24 12:04:39 +0200

    Added new assignments

--- pe7_4.py
+++ pe7_4.py
@@ -1,21 +1,29 @@
 import sys
+import datetime
+import time
 
 def Menu():
-    i = int(input('Kies een optie:\n1. Nieuwe naam toevoegen\n2. Huidige lijst inzien\n3. Sluiten\n'))
+    i = int(input('Kies een optie:\n1. Nieuwe naam toevoegen\n2. Huidige lijst inzien\n3. Bestand legen\n4. Sluiten\n'))
     if i == 1:
         NewName()
     if i == 2:
         ReadNames()
     if i == 3:
+        ClearFile()
+    if i == 4:
         sys.exit(0)
-    if i != 1 or i != 2 or i != 3:
+    if i != 1 or i != 2 or i != 3 or i != 4:
         print('\nDeze optie bestaat niet.')
         Menu()
 
 def NewName():
+    #Current time & date
+    vandaag = datetime.datetime.today()
+    s = vandaag.strftime("%a %d %b %Y, %T, ")
+
     new_name = input('Welke naam wilt u toevoegen?\n')
     with open('hardlopers.txt', 'a') as f:
-        f.write(new_name + '\n')
+        f.write('{}{}\n'.format(s, new_name))
         print('{} succesvol toegevoegd.\n'.format(new_name))
     Menu()
 
@@ -27,5 +35,9 @@ def ReadNames():
         print('Het bestand bestaat niet.\n')
     Menu()
 
+def ClearFile():
+    open('hardlopers.txt', 'w').close()
+    print('Bestand geleegd.\n')
+    Menu()
 
 Menu()
